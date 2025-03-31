1844640bb7ba9aa0215fdddcc2ba4ccf233011f2
Serhii Chornii <chorniiserhii06@gmail.com>
2025-03-31 22:54:01 +0300

    created new branch

---
+++ Lab4/zad3.py
@@ -0,0 +1,14 @@
+history = []
+
+while True:
+    text = input("Dodaj tekst:\n>  ")
+    match text.strip().lower():
+        case 'undo':
+            if not history:
+                print("Cofanie nie jest już możliwe!!!")
+                continue
+            print(history.pop())
+        case 'exit':
+            break
+        case _:
+            history.append(text)
