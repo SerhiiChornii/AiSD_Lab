e3e99e01a173bc85761cd07eaebfe43d6aea0e0a
Serhii Chornii <w72224@student.wsiz.edu.pl>
2025-04-01 15:02:50 +0200

    added zad4 to Lab5

---
+++ Lab5/zad4.py
@@ -0,0 +1,31 @@
+import heapq
+
+zadania = []
+# zadania = heapq.heapify(zadania)
+
+menu = '''\nWybierz działanie:
+1. Dodaj zadanie z priorytetem
+2. Obsłuż zadanie o najwyższym priorytecie
+3. Pokaż kolejkę zadań
+4. Zakończ działanie programu\n'''
+
+while True:
+    inpt = input("Wpisz działanie: ")
+    match inpt.strip().lower():
+        case '1':
+            zadanie = input("Wpisz zadanie: ")
+            priorytet = int(input("Wpisz priorytet zadania: "))
+            heapq.heappush(zadania, (zadanie, priorytet))
+        case '2':
+            print(heapq.heappop(zadania))
+        case '3':
+            if zadania:
+                for i in zadania:
+                    print(i)
+        case 'menu' | 'help' | '-h':
+            print(menu)
+        case 'exit' | '4':
+            print("Zakończenie roboty programu")
+            break
+        case _ :
+            print("Wprowadzono niepoprawne działanie")
