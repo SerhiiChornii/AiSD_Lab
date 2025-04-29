8882a71b7587d35fc54a17b7afe04f121be1ec4a
Serhii Chornii <w72224@student.wsiz.edu.pl>
2025-04-29 17:53:12 +0200

    lab6

---
+++ Lab6/zad1.py
@@ -0,0 +1,24 @@
+
+players_n = int(input("Wpisz liczbę graczy: "))
+scores = []
+
+for i in range(1, players_n+1):
+    score = int(input(f"Wpisz wynik gracza {i}: "))
+    scores.append(score)
+
+
+def bubble_sort(l):
+    n = len(l)
+    for i in range(n):
+        for j in range(n):
+            if l[j] > l[i]:
+                l[j], l[i] = l[i], l[j]
+    return l
+
+
+scores_s = bubble_sort(scores)
+
+print(f"Wyniki graczy: {scores}")
+print(f"Posortowane wyniki graczy: {scores_s}")
+print(f"Największy wynik: {scores_s[-1]}")
+print(f"Najmniejszy wynik: {scores_s[0]}")
